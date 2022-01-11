585953db57a258aa904e4e3ddf2339397c8245ab
Puppy2Git <alexander.garcia2722@gmail.com>
2022-01-11 10:48:09 -0600

    Dev Log 1
    And so Collisions Begin

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+#Important Panda3D Imports
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
+from panda3d.core import CollisionTraverser, CollisionHandlerPusher
+
+#Custom Imports
+import elevators
+import cogs
+import obstacles
+from props import propos
+import toons
 
 class AppGame(ShowBase):
     def __init__(self):
-        super().__init__()
+        super().__init__()#Inits the ShowBase
+        self.cTrav = CollisionTraverser()#Added the Collision Traverser
+        self.pusherh = CollisionHandlerPusher()#Adds a Collision Hanlder for Pushing
+        self.test = propos(self,0,[0,5,0])#Inits Obj 1
+        self.test2 = propos(self,0,[0,0,0])#Inits Obj 2
+        self.cTrav.addCollider(self.test2.nodePath, self.pusherh)#Adds Collider NotePath and Collision Handler to traverser
+        
+        #Note: may need to add back: ,self.drive.node()
+        self.pusherh.addCollider(self.test2.nodePath, self.test2.avatar)#Adds NodePath, Avatar, and base note of the collider
+        self.test2.fluidMove()#Move loops the character
 
 
 
--- props.py
+++ props.py
@@ -1,14 +1,40 @@
 from direct.actor.Actor import Actor
+from direct.showbase.DirectObject import DirectObject
+import panda3d
 from panda3d.core import CollisionSphere
-from panda3d.core import NodePath
-class propos:
+from panda3d.core import CollisionNode
+from panda3d.core import Point3
+class props_dicts:
+    #Model location, model animation locations, model radius
+    models = [["models/panda",{"walk":"models/panda-walk"},5]]
+
+class propos(DirectObject):
     position = []
-    model = None
+    avatar = None
     base = None
     collider = None
-    def __init__(self, base, model, rad):
-        self.base = base
-        self.model = model
-        self.collider = CollisionSphere(0,0,0, rad)
-        self.model.addSolid(self.collider)
+    def __init__(self, base, modelN = 0, pos = [0,0,0]):
+        super().__init__()
+        self.position = pos# Set position of class
+        self.base = base # To get Base
+        self.avatar = Actor(props_dicts.models[modelN][0],props_dicts.models[modelN][1]) #New Avatar
+        self.avatar.reparentTo(base.render) # Set avatar tied to game
+        self.avatar.setPos(pos[0],pos[1],pos[2])#Setting the position of the Actor
+        self.nodePath = self.avatar.attachNewNode(CollisionNode("cnode"))#New collision Node
+        self.collider = CollisionSphere(0,0,5, props_dicts.models[modelN][2]) # create colision sphere
+        self.nodePath.node().addSolid(self.collider) # Adding collider to avatar
+        self.nodePath.show()
+
+
+    def setPos(self,pos=[0,0,0],change=False):
+        if not change:
+            self.position = pos
+        else:
+            self.position += pos
+        self.avatar.setPos(pos[0],pos[1],pos[2])
         
+
+    def fluidMove(self):
+        self.avatar.posInterval(5, Point3(20, 0, 0), startPos=Point3(-20, 0, 0), fluid=1).loop()
+    def getPos(self):
+        return self.position
